709b66be7020f8825131bd8e6059422c4cf1ec2d
Kajol Gupta <kajol.gupta@speridian.com>
2024-01-15 23:58:44 +0530

    Add existing project files to Git

---
+++ src/Learning/test1.java
@@ -0,0 +1,18 @@
+package Learning;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	public void personalLoan()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void homeLoan()
+	{
+		System.out.println("Welcome");
+	}
+
+}
